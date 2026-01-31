1d1ab476a28330df4c276be63fc7f9477f666eb7
paris <paris@macbookpro.mynetworksettings.com>
2026-01-31 13:44:07 -0500

    getInitialGame

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import { createGame, makeMove } from "./tic-tac-toe";
+
 function App() {
-  return <div>Hello World</div>;
+  let [gameState, setGameState] = useState(getInitialGame())
+
+  // TODO: display the gameState, and call `makeMove` when a player clicks a button
+  return <div>Hello World! current player: {gameState.currentPlayer}</div>;
+}
+
+function getInitialGame() {
+  let initialGameState = createGame()
+  makeMove(initialGameState, 3)
+  makeMove(initialGameState, 0)
+  return initialGameState
 }
 
 export default App;
